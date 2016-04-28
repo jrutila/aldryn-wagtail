a8908e4738c4851d98944002e6f881e75a6dc5ec
christianbertschy <christian.bertschy@divio.ch>
2016-04-28 10:29:12 +0200

    Initial Wagtail test

--- urls.py
+++ urls.py
@@ -1,12 +1,35 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
 
+from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
+from wagtail.wagtailcore import urls as wagtail_urls
+from wagtail.contrib.wagtailapi import urls as wagtailapi_urls
+
+from demo import views
 
 urlpatterns = [
-    # add your own patterns here
-] + aldryn_addons.urls.patterns() + i18n_patterns(
-    # add your own i18n patterns here
-    *aldryn_addons.urls.i18n_patterns()  # MUST be the last entry!
-)
+    url(r'^django-admin/', include(admin.site.urls)),
+    url(r'^admin/', include(wagtailadmin_urls)),
+    url(r'^documents/', include(wagtaildocs_urls)),
+    url(r'search/$', views.search, name='search'),
+    url(r'^api/', include(wagtailapi_urls)),
+    # For anything not caught by a more specific rule above, hand over to
+    # Wagtail's serving mechanism
+    url(r'', include(wagtail_urls)),
+]
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.views.generic.base import RedirectView
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'demo/images/favicon.ico'))
+    ]
