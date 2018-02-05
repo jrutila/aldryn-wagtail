71faaa2f9bf125c44def747001593397fb0a2ce5
Daniele Procida <daniele@vurt.org>
2018-02-05 07:52:57 +0000

    Updated to install Wagtail 1.13.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     platforms=['OS Independent'],
     install_requires=(
         'aldryn-addons',
-        'wagtail==1.12',
+        'wagtail==1.13.1',
     ),
     include_package_data=True,
     zip_safe=False,
